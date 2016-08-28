73bddbdbbce912bc9fa3ee04addf716a4c54f1f6
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-27 18:25:01 -0700

    creates game prompt function

--- app.js
+++ app.js
@@ -2,25 +2,33 @@ var player1carImage = "http://rlv.zcache.com/sports_car_cutout-r416d3d66db574d74
 var player2carImage = "http://rlv.zcache.com/1959_retro_black_cutout-r3a9a0c3f0e934ea5ad725cb50c0e666b_x7sai_8byvr_324.jpg";
 
 $(document).ready(function() {
-//all the code to manipulate the DOM
-//set up game board
-
-//$(".game-start").show();
-
-$(".game-start").on("click", function handleClick() {
   var currentGame = new Game;
-  $(".game-start-button").hide();
-  $("#button-wrapper").append("<h2>Ready?</h2>");
-  $("#button-wrapper").append("<h2>Set...</h2>");
-  $("#button-wrapper").append("<h2>Go!!!</h2>");
+  var allGamePrompts = $(".game-prompt");
+  var currentGamePrompt = 0;
+
+  $(".game-start").on("click", function handleClick() {
+
+    function changeGamePrompts () {
+      $(allGamePrompts[currentGamePrompt]).fadeIn(100, function () {
+        if(currentGamePrompt == allGamePrompts.length - 1) {
+          currentGamePrompt = 0;
+        }
+        else {
+          currentGamePrompt++;
+        }
+        $(allGamePrompts[currentGamePrompt]).fadeOut(100);
+      });
+    }
+    $(currentGamePrompt);
+    var gamePromptTimer = setInterval(changeGamePrompts, 1000);
 });
 
-  $(" ").on("keypress", function() {
+//  $(" ").on("keypress", function() {
     //tap spacebar
-    increaseScore(1)
+//    increaseScore(1)
     //hold spacebar
-    increaseScore(2)
-  });
+//    increaseScore(2)
+//  });
 
   //if keypress a = player1
   //var key = a.which;
@@ -34,18 +42,18 @@ function Car (carImage) {
   this.image = carImage;
   this.score = 0;
   console.log("car");
-}
+};
 
 Car.prototype.increaseScore = function (num) {
   this.score += num;
 //  $(this).append(score);
   console.log("increaseScore");
-}
+};
 
 function Game () {
   whiteCar = new Car(player1carImage);
   blackCar = new Car(player2carImage);
-}
+};
 
 //Game.prototype.raceClock = function () {
   //
